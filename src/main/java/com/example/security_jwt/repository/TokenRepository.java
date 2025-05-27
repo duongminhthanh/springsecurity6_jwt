22b409b0a10c8f58e1507565a3b31d55a66e1524
duongminhthanh <duongminhthanh1999td@gmail.com>
2025-05-27 16:57:07 +0700

    complete project

---
+++ src/main/java/com/example/security_jwt/repository/TokenRepository.java
@@ -0,0 +1,9 @@
+package com.example.security_jwt.repository;
+
+import com.example.security_jwt.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+}
